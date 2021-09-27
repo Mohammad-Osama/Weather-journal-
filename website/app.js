15117a47c9ff2dbb6b8a5b17dc1489fb13809c65
Mohammad <moh.osama84x@gmail.com>
2021-09-27 10:23:59 +0200

    Updating DOM elements

--- website/app.js
+++ website/app.js
@@ -9,14 +9,15 @@ const key = '1c82219adec930f1fbfb9a2bcc3f0ab2' ;
  const generateButton = document.getElementById('generate') ; 
  generateButton.addEventListener('click',weather) ;         // 16505
  async function  weather() {   // asynchronous function to run across the whole code  
+    const feelings  = document.getElementById('feelings').value ; 
     const zip = document.getElementById('zip').value ;   
-    console.log(zip);
+    // console.log(zip);
        // getting the data from the provided weather api 
     const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${key}` ;
     const response = await fetch(url) ;
     const data = await response.json() ;  // converting from json format  
-    console.log(data);
-    console.log(data.main.temp);   /////////////////////////////////////////
+    // console.log(data);
+    // console.log(data.main.temp);   
            // posting data to the server 
     await fetch('/save',{   // using the post route to post the fetched data
       method: "POST",              
@@ -34,30 +35,13 @@ const key = '1c82219adec930f1fbfb9a2bcc3f0ab2' ;
     console.log (reqsJson) ;  */
 
 
-    const calledData = await fetch('/call') ; 
+    const calledData = await fetch('/call') ; // using the get route to get data from server 
     const final = await calledData.json()
-    console.log (final.temp) ; 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+     // console.log (final.temp) ; 
+       //changing the dom elements with the required data 
+    document.getElementById('date').innerHTML     = 'date                   '+                    newDate ; 
+    document.getElementById('temp').innerHTML     = 'temperature'+  final.temp ;
+    document.getElementById('content').innerHTML  = 'you were feeling'+  feelings ; 
 
  }
 
